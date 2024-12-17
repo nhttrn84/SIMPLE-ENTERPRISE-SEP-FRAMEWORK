07935c0cf626d2230023017355e22064bde85701
nhttrn84 <nhttrn84@gmail.com>
2024-12-17 23:34:06 +0700

    initiallize

---
+++ SimpleEnterpriseFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using SimpleEnterpriseFramework.DBSetting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleEnterpriseFramework
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Database databases = new Database();
+            List<string> list = databases.GetDatabaseNames(); // Lấy cái CSDL từ máy lên
+            SelectDB sldb = new SelectDB(list); // Render list CSDL lên comboBox
+            Application.EnableVisualStyles();
+            Application.Run(sldb);
+        }
+    }
+}
